a7b697667cdb24f5a0288f81852a458d12a75098
godvishal123 <98732691+godvishal123@users.noreply.github.com>
2023-09-19 19:20:50 +0000

    data

---
+++ Basic/dataTypes.js
@@ -0,0 +1,8 @@
+let name ="vishal"
+let age= 20
+
+
+
+// object
+console.log(typeof null);
+console.log("aman");
---
+++ Basic/variables.js
@@ -0,0 +1,15 @@
+const accId=144
+let accEmail= "v@gmail.com"
+var accPass= "123456"
+accCity = "kolkata"
+
+// accId =2
+
+
+accEmail ="vis@gmail.com"
+accPass = "125230"
+accCity = "howrah"
+
+console.log(accId);
+console.table([accId , accEmail,accPass, accCity])
+
